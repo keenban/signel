9ee7a919965c33011fe3699310cb2c28c04c16b8
Keenan Salandy <keenban@proton.me>
2026-01-21 18:54:55 -0500

    remove keywords

--- sign.el
+++ sign.el
@@ -7,7 +7,6 @@
 ;; URL: https://github.com/keenban/signel
 ;; Version: 0.1.0
 ;; Package-Requires: ((emacs "27.1"))
-;; Keywords: comm, signal, chat, external
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
